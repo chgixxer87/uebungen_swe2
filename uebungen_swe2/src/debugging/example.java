0f57b55efd10ea3b816182fdde76e7b95f58a628
Joel <Joel@192.168.178.46>
2017-02-26 17:18:23 +0100

    1. commit

---
+++ uebungen_swe2/src/debugging/example.java
@@ -0,0 +1,35 @@
+package debugging;
+
+
+public class example {
+	
+
+	public static void main(String[] args) {
+		
+        int[] numsOut;
+        int value= 0;
+        
+        int [] nums = new int [5];
+        
+        for (int i =0; i<nums.length;i++){
+        	nums[i]= value+1;
+        }
+
+        
+
+        // Create the second array
+        int lengthNumsOut = nums.length / 2; // error
+        numsOut = new int[lengthNumsOut];
+
+        // Process the first array into the second
+        for (int i = 0; i < lengthNumsOut; i++) {
+            numsOut[i] = nums[i] + nums[nums.length - i]; // error
+        }
+
+        // Print the result
+        System.out.println(numsOut); // error
+    }
+
+	}
+
+
